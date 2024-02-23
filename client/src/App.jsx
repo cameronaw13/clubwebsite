168ff2edc79e761294a5ac079ccf77d2848f3f45
Joshua Rashtian <joshrashtian1@gmail.com>
2024-02-23 12:20:38 -0800

    Nav Bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { 
-  BrowserRouter as Router, 
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
   createBrowserRouter,
   RouterProvider,
-  Routes, 
-  Route, 
-  Link 
-} from 'react-router-dom';
-
-import './App.css';
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Foot from "../src/components/Footer/Foot.tsx";
+import "./App.css";
 
 // Add new components here!
-import HomePage from './components/HomePage/HomePage.tsx';
-import Projects from './components/Projects/Projects.tsx';
+import HomePage from "./components/HomePage/HomePage.tsx";
+import Projects from "./components/Projects/Projects.tsx";
 
 // Add new paths here!
 const router = createBrowserRouter([
@@ -23,9 +23,12 @@ const router = createBrowserRouter([
 function App() {
   return (
     <>
-      <RouterProvider router={router}/>
+      <Foot />
+      <section style={{ marginTop: '10%'}}>
+      <RouterProvider router={router} />
+      </section>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/components/Footer/Foot.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Foot.css";
+
+const tabs = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+];
+
+const Foot: React.FC = () => {
+  return (
+    <nav className="Foot">
+      <main className="FootContainer">
+        <p>
+          <h1 style={{ color: "white" }}>COC Tech Club</h1>
+        </p>
+        <section style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+          {tabs.map((tab) => {
+            return (
+              <a href={tab.path} className="NavItems" key={tab.name}>
+                {tab.name}
+              </a>
+            );
+          })}
+        </section>
+      </main>
+    </nav>
+  );
+};
+
+export default Foot;
