c003381dec2f7acdd6b8b6e100fb76c165a221dc
cameronaw13 <cameronaw13@proton.me>
2024-04-01 11:40:43 -0700

    add firebase integration in events/projects

--- client/src/components/Events/EventsCard.tsx
+++ client/src/components/Events/EventsCard.tsx
@@ -1,29 +1,43 @@
 import React from 'react';
+import { Timestamp } from "firebase/firestore";
 
 interface EventsCardInterface {
     name: string,
-    date: string,
-    startTime: string,
-    endTime: string,
-    location: string[],
+    startTime: Timestamp,
+    endTime?: Timestamp,
+    location?: string[],
     link?: string
 }
 
 const EventsCard: React.FC<EventsCardInterface> = ({
     name = "Event Title",
-    date = "January 1st, 1970",
-    startTime = "12:00am",
-    endTime = "11:59pm",
-    location = ["N/A"],
-    link = ""
+    startTime = Timestamp.fromDate(new Date(0)),
+    endTime,
+    location,
+    link
 }) => {
     return (
         <div className="EventCard">
             <div className="EventCardText">
-                <p className="EventCardName">{name}</p>
-                <p><b>Date: </b>{date}</p>
-                <p><b>Time: </b>{startTime} - {endTime}</p>
-                <p><b>Location: </b>{link ? <a href={link}>{location.join(", ")}</a> : location.join(", ")}</p>
+                <p className="EventCardName">{link ? <a href={link}>{name}</a> : name}</p>
+                <p><b>Date: </b>{startTime.toDate().toLocaleDateString("en-US", {
+                    month:"long",
+                    day:"2-digit",
+                    year:"numeric"
+                })}</p>
+                <p><b>Time: </b>{endTime ? startTime.toDate().toLocaleTimeString("en-US", {
+                    hour:"numeric",
+                    minute:"2-digit"
+                }) + " - " + endTime.toDate().toLocaleTimeString("en-US", {
+                    hour:"numeric",
+                    minute:"2-digit",
+                    timeZoneName:"short"
+                }) : startTime.toDate().toLocaleTimeString("en-US", {
+                    hour:"numeric",
+                    minute:"2-digit", 
+                    timeZoneName:"short"
+                })}</p>
+                <p><b>Location: </b>{location ? location.join(", ") : null }</p>
             </div>
         </div>
     )
