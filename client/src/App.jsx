0f355a285cfda665a64431eea90d49bf49f7e158
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-04-19 11:29:22 -0700

    added join form to router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,25 +8,24 @@ import {
 import "./App.css";
 
 // components
+import Header from "./components/Header/Header.tsx";
 import HomePage from './components/HomePage/HomePage.tsx';
 import Projects from './components/Projects/Projects.tsx';
 import Events from './components/Events/Events.tsx';
-import Foot from "./components/Footer/Foot.tsx";
-
-// add new paths here!
-const repoName = `COC-ACM-Website`
+import JoinForm from './components/JoinForm/JoinForm.tsx'
 
 // Add new paths here!
 const router = createHashRouter([
   { path: "/", element: <HomePage /> },
   { path: "/projects", element: <Projects /> },
   { path: "/events", element: <Events />},
+  { path: "/join", element: <JoinForm />}
 ]);
 
 function App() {
   return (
     <>
-      <Foot />
+      <Header />
       <section style={{ marginTop: '10%'}}>
       <RouterProvider router={router} />
       </section>
