51cbd1b0abf9a822739631b0ca7b4fd9aba28e60
cameronaw13 <cameronawichman@gmail.com>
2024-05-03 11:54:18 -0700

    start the menu

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState, /* useRef */ } from "react";
 import { useLocation } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
+
 import "./Header.css";
+import "../../assets/Header/HeaderAssets.css";
+import { MenuIcon } from "../../assets/Header/MenuIcon.tsx";
+import Menu from "./Menu.tsx";
 
 const repoName = `clubwebsite`
-
 const tabs = [
   {
     name: `Home`,
@@ -21,13 +24,14 @@ const tabs = [
   {
     name: `Join`,
     path: `/join`,
-  }
+  },
 ];
 
 const Header: React.FC = () => {
   const location = useLocation();
   const isHomePage = () => { return location.pathname === "/" }
   const [opacity, setOpacity] = useState(isHomePage() ? 0 : 1)
+  const [isMenu, setMenu] = useState(false);
   //const nodeRef = useRef(null);
     
   useEffect(() => {
@@ -50,34 +54,48 @@ const Header: React.FC = () => {
     }
   }
 
+  const handleMenu = () => {
+    setMenu(!isMenu);
+  }
+
   return (
-    <nav className="Header">
-      <CSSTransition
-        in={location.pathname === "/"}
-        timeout={{
-          enter: 100,
-          exit: 600,
-        }}
-        classNames="headerbg"
-        //nodeRef={nodeRef}
-      >
-        <div className="HeaderBackground" style={{ opacity: opacity }}/>
-      </CSSTransition>
-      <main className="HeaderContainer">
-        <a href={`/${repoName}/#/`}>
-          <h1 className="HeaderTitle">COC Tech Club</h1>
-        </a>
-        <section style={{ display: "flex", flexDirection: "row" }}>
-          {tabs.map((tab) => {
-            return (
-              <a href={`/${repoName}/#` + tab.path} className="NavItems" key={tab.name}>
-                {tab.name}
-              </a>
-            );
-          })}
-        </section>
-      </main>
-    </nav>
+    <>
+      <nav className="Header">
+        <CSSTransition
+          in={location.pathname === "/"}
+          timeout={{
+            enter: 100,
+            exit: 600,
+          }}
+          classNames="headerbg"
+          //nodeRef={nodeRef}
+        >
+          <div className="HeaderBackground" style={{ opacity: opacity }}/>
+        </CSSTransition>
+        <main className="HeaderContainer">
+          <a href={`/${repoName}/#/`}>
+            <h1 className="HeaderTitle">COC Tech Club</h1>
+          </a>
+          <section style={{ display: "flex", flexDirection: "row" }}>
+            {tabs.map((tab) => {
+              return (
+                  <a href={`/${repoName}/#` + tab.path} className="NavItems" key={tab.name}>
+                    {tab.name}
+                  </a>
+              );
+            })}
+            <button onClick={handleMenu} className="NavMenu">
+              <MenuIcon />
+            </button>
+          </section>
+        </main>
+      </nav>
+      { isMenu ? (
+        <Menu />
+      ) : (
+        ''
+      )}
+    </>
   );
 };
 
