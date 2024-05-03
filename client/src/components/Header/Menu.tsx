83d555c7c98187ba4e51b36109c1a432216f2432
cameronaw13 <cameronawichman@gmail.com>
2024-05-03 13:32:14 -0700

    fix untracked files

---
+++ client/src/components/Header/Menu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Menu: React.FC = (tabs) => {
+  return (
+    <div style={{ position: "fixed", color: "#fff", zIndex: "1" }}>
+      THIS IS A MENU!
+    </div>
+  );
+}
+
+export default Menu;
